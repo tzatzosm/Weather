6ad7994c63a51e935ba768b75ad2ebffb1abcc26
Marsel Tzatzos <mtz@captureone.com>
2021-11-27 11:33:19 +0200

    Initial Commit

---
+++ Weather/AppDelegate.swift
@@ -0,0 +1,30 @@
+//
+//  AppDelegate.swift
+//  Weather
+//
+//  Created by Marsel Tzatzos on 27/11/2021.
+//
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+    func applicationSupportsSecureRestorableState(_ app: NSApplication) -> Bool {
+        return true
+    }
+
+
+}
+
---
+++ Weather/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  Weather
+//
+//  Created by Marsel Tzatzos on 27/11/2021.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
